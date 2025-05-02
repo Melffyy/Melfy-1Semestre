4939a954636f238f5d1ccc5dd4eff09ec8209514
nicole <nicole.goncalves6@etec.sp.gov.br>
2025-05-01 23:18:59 -0300

    página de abertura

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Página carregada! Aguardando 5 segundos...");
+    
+    setTimeout(function() {
+      window.location.href = "";
+    }, 3000); 
+  });
+  
