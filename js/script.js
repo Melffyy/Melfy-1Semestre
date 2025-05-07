67e9a29fe7c698517c34467284e7e395f28d5475
nicole <nicole.goncalves6@etec.sp.gov.br>
2025-05-06 21:48:19 -0300

    organizando

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("Página carregada! Aguardando 5 segundos...");
+    console.log("Página carregada! Aguardando 3 segundos...");
     
     setTimeout(function() {
       window.location.href = "";
