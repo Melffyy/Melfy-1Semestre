d33c5bdc91b4940b4dd23c166b58762818fca4e2
nicole <nicole.goncalves6@etec.sp.gov.br>
2025-05-24 14:07:54 -0300

    Pg editarProdutos da confeiteira

---
+++ confeiteira/js/meusProdutos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.botao-editar').addEventListener('click', function() {
+    window.location.href = './editarProdutos.html';
+ });
+});
+
