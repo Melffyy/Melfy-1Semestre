eacebd58a973583bc1ca6669ace729f586386de3
nicole <nicole.goncalves6@etec.sp.gov.br>
2025-05-27 21:29:51 -0300

    Testando exibição dos produtos

--- confeiteira/js/meusProdutos.js
+++ confeiteira/js/meusProdutos.js
@@ -1,6 +1,46 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.botao-editar').addEventListener('click', function() {
     window.location.href = './editarProdutos.html';
  });
+});*/
+
+document.addEventListener("DOMContentLoaded", function() {
+    let produtos = JSON.parse(localStorage.getItem('Produtos')) || [];
+
+    if (produtos.length > 0) {
+        const cardsWrapper = document.querySelector('.cards-wrapper');
+        
+        produtos.forEach(produto => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            
+            card.innerHTML = `
+                <div class="border-card">
+                    <div class="headerNovidade">
+                        <img src="${produto.foto}" alt="Logo Loja" class="logoLoja">
+                        <span class="marca">${produto.idLoja}</span>
+                    </div>
+                    <img src="${produto.foto}" alt="Imagem do Produto" class="imagem-produto">
+                    <div class="descricao">
+                        <h3>${produto.nome}</h3>
+                        <p>${produto.subtitulo}</p>
+                    </div>
+                    <div class="footerNovidades">
+                        <div class="preco">
+                            <span class="icone-preco">$</span>
+                            <span class="valor">${produto.preco}</span>
+                        </div>
+                        <button class="botao-editar">Editar</button>
+                    </div>
+                </div>
+            `;
+            
+            cardsWrapper.appendChild(card);
+        });
+    } else {
+        const cardsWrapper = document.querySelector('.cards-wrapper');
+        cardsWrapper.innerHTML = '<p>Nenhum produto encontrado.</p>';
+    }
 });
+
 
