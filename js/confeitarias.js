40da544e40d1ad6777de48dbe4d0a0a47fe567f3
nicole <nicole.goncalves6@etec.sp.gov.br>
2025-05-29 20:22:48 -0300

    implementando lógica na pg de confeitarias

---
+++ js/confeitarias.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () { 
+    let produtos = JSON.parse(localStorage.getItem('Produtos')) || [];
+    let lojas = JSON.parse(localStorage.getItem('Lojas')) || [];
+
+    if (produtos.length > 0) {
+        const cardsWrapper = document.querySelector('.cards-wrapper');
+
+        produtos.forEach(produto => {
+            const idLojaProduto = parseInt(produto.idLoja);
+            const loja = lojas.find(l => l.idLoja === idLojaProduto);
+
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            card.innerHTML = `
+                <div class="border-card">
+                    <div class="headerNovidade">
+                        <img src="${loja?.fotoPerfil || ''}" alt="Logo da Loja" class="logoLoja">
+                        <span class="marca">${loja?.nomeLoja || 'Loja Desconhecida'}</span>
+                    </div>
+                    <img src="${produto.foto}" alt="Imagem do Produto" class="imagem-produto">
+                    <div class="descricao">
+                        <h3>${produto.nome}</h3>
+                        <p>${produto.subtitulo}</p>
+                    </div>
+                    <div class="footerNovidades">
+                        <div class="preco">
+                            <span class="icone-preco">$</span>
+                            <span class="valor">${produto.preco}</span>
+                        </div>
+                        <button class="botao-comprar">Comprar</button>
+                    </div>
+                </div>
+            `;
+
+            const botaoEditar = card.querySelector('.botao-comprar');
+            botaoEditar.addEventListener('click', function () {
+                window.location.href = `./.html?idProduto=${produto.idProduto}`;
+            });
+
+            cardsWrapper.appendChild(card);
+        });
+    } else {
+        const cardsWrapper = document.querySelector('.cards-wrapper');
+        cardsWrapper.innerHTML = '<p>Nenhum produto encontrado.</p>';
+    }
+});
