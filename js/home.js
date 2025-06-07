77b8e913f849be9e8467fe6fb7566c05621584b0
nicole <nicole.goncalves6@etec.sp.gov.br>
2025-06-07 20:28:13 -0300

    home

--- js/home.js
+++ js/home.js
@@ -37,3 +37,29 @@ function pesquisar() {
       alert("Ocorreu um erro ao buscar o endereço.");
     });
 }
+
+const lojas = JSON.parse(localStorage.getItem('Lojas')) || [];
+const container = document.getElementById('lojas-container');
+
+lojas.forEach(loja => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.innerHTML = `
+    <div class="produto">
+      <img class="logoLoja" src="${loja.fotoPerfil}" alt="${loja.nomeLoja}">
+      <div>
+        <h4 class="marca">${loja.nomeLoja}</h4>
+        <p>${loja.descricao}</p>
+      </div>
+    </div>
+  `;
+
+  card.addEventListener('click', () => {
+    localStorage.setItem('idLojaSelecionada', loja.idLoja);
+    window.location.href = 'loja.html';
+  });
+
+  container.appendChild(card);
+});
+
