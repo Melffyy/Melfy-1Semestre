2e97c838d49d3d7fc073c557875305fc541fbe82
nicole <nicole.goncalves6@etec.sp.gov.br>
2025-06-07 15:09:37 -0300

    respansividade + modo escuro

--- js/carrossel.js
+++ js/carrossel.js
@@ -18,3 +18,21 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+
+const btn = document.getElementById('toggle-dark-mode');
+
+window.addEventListener('DOMContentLoaded', () => {
+  const darkModeEnabled = localStorage.getItem('darkMode') === 'true';
+
+  btn.classList.toggle('active', darkModeEnabled);
+  document.body.classList.toggle('dark-mode', darkModeEnabled);
+  btn.setAttribute('aria-pressed', darkModeEnabled);
+});
+
+btn.addEventListener('click', () => {
+  const isActive = btn.classList.toggle('active');
+  document.body.classList.toggle('dark-mode', isActive);
+  btn.setAttribute('aria-pressed', isActive);
+  localStorage.setItem('darkMode', isActive);
+});
